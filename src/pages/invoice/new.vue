029222df21b643a13199639b1b17aaf98ee91b21
brahimABD98 <brahim.abderrahim@esprit.tn>
2022-11-17 15:47:56 +0100

    invoiceline form

--- src/pages/invoice/new.vue
+++ src/pages/invoice/new.vue
@@ -1,17 +1,25 @@
-<script setup>
+<script >
 import { ref } from 'vue'
-const submitted = ref(false)
-const submitHandler = async () => {
-  // Let's pretend this is an ajax request:
-  await new Promise((r) => setTimeout(r, 1000))
-  submitted.value = true
+export default {
+  setup() {
+    // const submitted = ref(false)
+
+    const value = ref();
+    const invoicelines = ref([])
+    const invoiceline = ref()
+    // console.log(value.value)
+    return {
+      value, invoicelines, invoiceline
+    }
+  }
 }
+
+
 </script>
 
 <template>
   <div>
-    <FormKit type="form" name="Invoice" id="registration-example" :form-class="submitted ? 'hide' : 'show'"
-      submit-label="Register" @submit="submitHandler" :actions="false" #default="{ value }">
+    <FormKit type="form" name="Invoice" id="registration-example" submit-label="Register" #default="{ value }">
       <h1>Facutre!</h1>
       <p>
         Remplissez les champs selon vos besoins
@@ -27,16 +35,27 @@ const submitHandler = async () => {
       <FormKit type="text" name="timbre" label="timbre"
         :validation="[['matches', /^(?:[1-9]\d*|0(?!(?:\.0+)?$))?(?:\.\d+)?$/]]" />
       <FormKit type="number" name="taux" label="taux tva global" validation="required" min="1" max="100" />
-      <!-- <FormKit id="repeater" name="invoiceline" type="repeater" label="invoiceline">
-        <FormKit type="text" label="produit" name="produit" validation="required" />
-      </FormKit> -->
 
-   
+
+
+      <FormKit name="invoicelines" v-model="invoicelines" type="list">
+        <FormKit type="group" name="invoiceline" v-model="invoiceline">
+
+          <FormKit type="text" name="produit" label="Produit" validation="required" />
+          <FormKit type="number" name="qte" label="quantite" validation="required" min="1" value="1" />
+          <FormKit type="text" name="htva" label="prix untaire hors tva"
+            :validation="[['matches', /^(?:[1-9]\d*|0(?!(?:\.0+)?$))?(?:\.\d+)?$/]]" />
+          <FormKit type="text" name="tva" label="tva"
+            :validation="[['matches', /^(?:[1-9]\d*|0(?!(?:\.0+)?$))?(?:\.\d+)?$/]]" />
+          <FormKit type="text" name="ttc" label="ttc"
+            :validation="[['matches', /^(?:[1-9]\d*|0(?!(?:\.0+)?$))?(?:\.\d+)?$/]]" />
+          <FormKit type="number" name="taux" label="taux tva" validation="required" value="20" />
+        </FormKit>
+      </FormKit>
+
       <FormKit type="submit" label="Register" />
       <pre wrap>{{ value }}</pre>
     </FormKit>
-    <div v-if="submitted">
-      <h2>Submission successful!</h2>
-    </div>
+
   </div>
 </template>
