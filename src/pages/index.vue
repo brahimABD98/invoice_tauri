c9da580b4cc7ef7b7100d9960c0ed2b0b39e2eb2
brahimABD98 <brahim.abderrahim@esprit.tn>
2022-12-02 12:15:44 +0100

    two way binding  form except for date

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ListFacture from '../components/ListFacture.vue';
+
+</script>
+
+<template >
+    <div class=" mx-10 flex flex-row w-full ">
+        <div class="flex-col w-full">
+            <router-link to="/invoice/new">
+                <button class="btn btn-outline btn-accent mb-6"> Ajouter une facture</button>
+            </router-link>
+            <ListFacture />
+        </div>
+    </div>
+</template>
